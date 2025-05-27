17c330b217c94011f287e22077fe38f169382658
Gourav Sen <gourav.s@turing.com>
2025-05-27 18:26:58 +0530

    interaction-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get "home/index"
   devise_for :users
   root "home#index"
--- db/migrate/20250527114750_devise_create_users.rb
+++ db/migrate/20250527114750_devise_create_users.rb
@@ -7,6 +7,20 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## Custom fields
+      t.string :username
+      t.string :anonymous_identifier
+      t.string :name
+      t.string :email_domain
+      t.text :bio
+      t.boolean :profile_visible, default: true
+      t.string :role, default: "user"
+      t.integer :mood
+      t.datetime :mood_updated_at
+      t.integer :compliments_sent_count, default: 0
+      t.integer :compliments_received_count, default: 0
+      t.integer :kudos_received_count, default: 0
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -14,35 +28,31 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
+      ## Trackable (uncomment if you want login tracking)
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
       # t.datetime :last_sign_in_at
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
 
-      ## Confirmable
+      ## Confirmable (uncomment if you want email confirmation)
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      # t.string   :unconfirmed_email
 
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      ## Lockable (uncomment if you want account locking)
+      # t.integer  :failed_attempts, default: 0, null: false
+      # t.string   :unlock_token
       # t.datetime :locked_at
-
-      t.string :name
-      t.string :email_domain
-      t.string :role
-      t.integer :mood
-      t.datetime :mood_updated_at
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username,             unique: true
+    add_index :users, :anonymous_identifier, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
