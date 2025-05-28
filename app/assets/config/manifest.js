ad8f403a20448a127f58a9421e1484406db94646
Gourav Sen <gourav.s@turing.com>
2025-05-28 17:49:25 +0530

    interaction-4

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,4 @@
 //= link_tree ../images
-//= link_tree ../builds
 //= link_directory ../stylesheets .css
-//= link importmap.js
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link_importmap
+//= link tailwind.css
