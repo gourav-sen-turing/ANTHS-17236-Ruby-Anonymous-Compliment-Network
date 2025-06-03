5e0c25af965be476ae87bc12faadf9c6defb5447
Gourav Sen <gourav.s@turing.com>
2025-06-03 17:15:38 +0530

    interaction-9

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,3 @@
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers", preload: true
-pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@3.7.1/dist/chart.esm.js"
-# Any other required JavaScript packages or libraries
+# Pin npm packages by running ./bin/importmap
 
-# If using stimulus-webpack-helpers
-pin "@hotwired/stimulus-webpack-helpers", to: "stimulus-webpack-helpers.js", preload: true
+pin "application"
