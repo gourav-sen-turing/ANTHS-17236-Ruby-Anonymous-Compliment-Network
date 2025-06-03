4ce19e303f5aade82e58cb3744a40b37e126d05e
Gourav Sen <gourav.s@turing.com>
2025-06-03 16:47:25 +0530

    interaction-7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,13 @@
 import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+// Initialize Stimulus application
+const application = Application.start()
+
+// Register controllers from controllers directory
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// Or if using the default controllers loading approach in Rails 7:
 import "controllers"
