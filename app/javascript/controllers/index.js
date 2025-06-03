7c584f4470c415a23e8932b739f5aaf043d4e352
Gourav Sen <gourav.s@turing.com>
2025-06-03 17:58:23 +0530

    interaction-10

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,4 @@
-import { application } from "./application"
-
-// Eager load all controllers defined in the import map under controllers/**/*_controller
+// Import and register all your controllers from the importmap via controllers/**/*_controller
+import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
